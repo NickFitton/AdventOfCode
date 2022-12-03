637308eb51a1d4b57198530ec1e72cc13a9d6419
Nick Fitton <nick.fitton@ovo.com>
2022-12-03 06:49:43 +0000

    Day 3

---
+++ 2022/3/main.ts
@@ -0,0 +1,48 @@
+import "https://deno.land/x/arrays@v1.0.21/mod.ts";
+
+export const getText = () => Deno.readTextFile("./input.txt");
+
+const textToCompartments = (text: string): [string, string][] =>
+  text
+    .split("\n")
+    .map((line) => [
+      line.slice(0, line.length / 2),
+      line.slice(line.length / 2),
+    ]);
+
+const itemToPriority = (text: string): number => {
+  const asciiValue = text.charCodeAt(0);
+  if (asciiValue < 91) {
+    return asciiValue - 38;
+  } else {
+    return asciiValue - 96;
+  }
+};
+
+export const priorityItemSums = (text: string): number => {
+  return textToCompartments(text).reduce((acc, compartment) => {
+    const repeat: string | undefined = compartment[0]
+      .split("")
+      .find((character) => compartment[1].includes(character));
+    if (!repeat) {
+      return acc;
+    }
+    return acc + itemToPriority(repeat);
+  }, 0);
+};
+
+const textToGroups = (text: string): [string[], string[], string[]][] => {
+  return text
+    .split("\n")
+    .map((line) => line.split(""))
+    .chunk(3) as [string[], string[], string[]][];
+};
+
+export const stickerAttachmentEfforts = (text: string): number => {
+  return textToGroups(text)
+    .map(([a, b, c]) =>
+      a.find((character) => b.includes(character) && c.includes(character))
+    )
+    .map((item) => (item ? itemToPriority(item) : 0))
+    .reduce((a, b) => a + b);
+};
